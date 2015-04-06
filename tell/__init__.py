8bdacdb7b3a0a7e99d4728b12a9f96b110d12413
Alex M. Lowe <lengau@gmail.com>
2015-04-06 16:34:29 -0400

    Protection for DBus failures.
    If dbus isn't running (e.g. a remote machine), notify2 will fail to
    initialise and will later fail the notification. Instead of crashing
    with a traceback here, we should safely handle it failing.

--- tell/__init__.py
+++ tell/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from __future__ import print_function
+
+from dbus.exceptions import DBusException
 import notify2
 import subprocess
 import sys
@@ -11,7 +14,13 @@ def main():
 
 	# TODO: Prevent tell from running as root.
 
-	notify2.init('Tell')
+	try:
+		notify2.init('Tell')
+	except DBusException:
+		print('\033[91mERROR\033[0m: Cannot connect to DBus. '  # red 'ERROR'
+		      'Notificatons will not work.',
+		      file=sys.stderr)
+		print('Proceeding anyway.', file=sys.stderr)
 	command_string = ' '.join(sys.argv[1:])
 	return_value = subprocess.call(command_string, shell=True)
 	if return_value == 0:
@@ -22,7 +31,19 @@ def main():
 		summary = 'Command completed with error'
 		body = 'Exit code %d from %s' % (return_value, sys.argv[1])
 		icon = 'dialog-error'
-	notify2.Notification(summary, body, icon).show()
+	try:
+		notify2.Notification(summary, body, icon).show()
+	except notify2.UninittedError:
+		print('Command complete. Could not notify due to DBus Error.',
+		      file=sys.stderr)
+		if return_value == 0:
+			return_string = '\033[92m%d\033[0m' % return_value  # Green value
+		else:
+			return_string = '\033[91m%d\033[0m' % return_value  # Red value
+		print('Return value was: %s' % return_string,
+		      file=sys.stderr)
+		exit(255)
+	exit(return_value)
 
 
 if __name__ == '__main__':
